7db996e4f045e7cfe327e2cccc3473b72017e675
49241036 <162901184+ynadtiy19@users.noreply.github.com>
2024-12-30 13:00:24 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,12 @@ import 'package:ai/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await ChatProvider.initHive();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => ChatProvider()),
